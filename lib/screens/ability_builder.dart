65c3dda985dee78b8a6e64616982f43f6e2ba6fb
Wilson, Marvin (jw9615) <jw9615@att.com>
2019-06-07 17:04:35 -0400

    prototyping the ability builder

--- lib/screens/ability_builder.dart
+++ lib/screens/ability_builder.dart
@@ -11,8 +11,8 @@ class AbilityBuilder extends StatelessWidget {
         title: Text("Ability Builder"),
       ),
       body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: AbilityPanel(),
-        padding: EdgeInsets.all(10.0),
       ),
     );
   }
@@ -25,6 +25,9 @@ class AbilityPanel extends StatefulWidget {
 
 class _AbilityPanelState extends State<AbilityPanel> {
   String _value;
+  bool _hasLevels = false;
+
+  get _costLabelText => _hasLevels ? 'Cost Per Level' : 'Cost';
 
   // Create a global key that will uniquely identify the Form widget and allow
   // us to validate the form
@@ -35,21 +38,94 @@ class _AbilityPanelState extends State<AbilityPanel> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              // hintText: 'Trait Name',
-              labelText: 'Trait Name',
+      child: Scrollbar(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              alignment: Alignment.bottomLeft,
+              child: TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Trait name',
+                  filled: true,
+                ),
+                onChanged: (value) {
+                  setState(
+                    () {
+                      if (value.isNotEmpty) {
+                        _value = value;
+                      }
+                    },
+                  );
+                },
+              ),
             ),
-            onChanged: (text) {
-              _value = text;
-            },
-          )
-        ],
+            Container(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                alignment: Alignment.bottomLeft,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: _costLabelText,
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                    Checkbox(
+                      onChanged: (bool value) {
+                        setState(() {
+                          _hasLevels = !_hasLevels;
+                        });
+                      },
+                      value: _hasLevels,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Text('Has Levels'),
+                    ),
+                    Expanded(
+                      child: Visibility(
+                        visible: _hasLevels,
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            labelText: 'Level',
+                            filled: true,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ))
+          ],
+        ),
       ),
     );
+
+    // return Column(
+    //   crossAxisAlignment: CrossAxisAlignment.start,
+    //   children: <Widget>[
+    //     TextField(
+    //         decoration: InputDecoration(
+    //           filled: true,
+    //           labelText:
+    //               'Trait (An advantage, disadvantage, attribute, secondary characteristic, quirk, skill, or technique)',
+    //         ),
+    //         onChanged: (text) {
+    //           _value = text;
+    //         }),
+    //     Text('Has Levels'),
+    //     Checkbox(
+    //       onChanged: (bool value) {
+    //         setState(() {
+    //           _hasLevels = !_hasLevels;
+    //         });
+    //       },
+    //       value: _hasLevels,
+    //     )
+    //   ],
+    // );
   }
 }
