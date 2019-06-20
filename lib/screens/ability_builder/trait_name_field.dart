70fbafe40aea15aee81a6581d717b42f56d3d8f5
Wilson, Marvin (jw9615) <jw9615@att.com>
2019-06-20 11:25:44 -0400

    Added text value and editing controller

--- lib/screens/ability_builder/trait_name_field.dart
+++ lib/screens/ability_builder/trait_name_field.dart
@@ -3,18 +3,20 @@ import 'package:gurps_ability_builder_web/widgets/common.dart';
 
 class TraitNameField extends StatelessWidget {
   final OnChanged<String> onChanged;
+  final String value;
 
-  const TraitNameField({Key key, this.onChanged, String value})
-      : super(key: key);
+  const TraitNameField({Key key, this.onChanged, this.value}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      // value: value,
       decoration: const InputDecoration(
         labelText: 'Trait name',
         filled: true,
       ),
       onChanged: onChanged,
+      controller: TextEditingController(text: value),
     );
   }
 }
